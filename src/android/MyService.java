248b3fbb3c5f3066f05b9193347dbfdc6eec3c23
Lionlancer <lionlancer@gmail.com>
2018-03-12 17:35:00 +0800

    updates

--- src/android/MyService.java
+++ src/android/MyService.java
@@ -15,12 +15,15 @@ public class MyService extends BackgroundService {
 	private final static String TAG = MyService.class.getSimpleName();
 	
 	private String mHelloTo = "World";
+	private String userID = "";
+	private Number updateCount = 0;
 
 	@Override
 	protected JSONObject doWork() {
 		JSONObject result = new JSONObject();
 		
 		try {
+			
 			SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss"); 
 			String now = df.format(new Date(System.currentTimeMillis())); 
 
@@ -28,6 +31,25 @@ public class MyService extends BackgroundService {
 			result.put("Message", msg);
 
 			Log.d(TAG, msg);
+			
+			if(this.userID != ""){
+				URL url = new URL("https://onesignal-3cdb8.firebaseio.com/users/"+this.userID+".json"); https://[PROJECT_ID].firebaseio.com/users/jack/name.json
+				URLConnection con = url.openConnection();
+				HttpURLConnection http = (HttpURLConnection)con;
+				http.setRequestMethod("POST"); // PUT is another valid option
+				http.setDoOutput(true);
+				
+				byte[] out = "{\"position\":{\"Accuracy\":\"auto\",\"Altitude\":\"auto\",\"Latitude\":\"Unknown\",\"Longitude\":\"Unknown\",\"Accuracy\":\"auto\",\"Timestamp\":\"auto\"},\"updateCount\":\"NA\"}" .getBytes(StandardCharsets.UTF_8);
+				int length = out.length;
+
+				http.setFixedLengthStreamingMode(length);
+				http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+				http.connect();
+				try(OutputStream os = http.getOutputStream()) {
+					os.write(out);
+				}
+				// Do something with http.getInputStream()
+			}
 		} catch (JSONException e) {
 		}
 		
@@ -51,6 +73,12 @@ public class MyService extends BackgroundService {
 		try {
 			if (config.has("HelloTo"))
 				this.mHelloTo = config.getString("HelloTo");
+			
+			if (config.has("userID"))
+				this.userID = config.getString("userID");
+			
+			if (config.has("updateCount"))
+				this.updateCount = config.getString("updateCount");
 		} catch (JSONException e) {
 		}
 		
