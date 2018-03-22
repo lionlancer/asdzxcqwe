e44a9cb9baf0e384e9dfd91e7e46c33a538b99c3
Lionlancer <lionlancer@gmail.com>
2018-03-22 23:17:33 +0800

    updated saving post

--- src/android/MyService.java
+++ src/android/MyService.java
@@ -48,7 +48,53 @@ public class MyService extends BackgroundService {
 			Log.d(TAG, msg);
 			Log.d("MyService", msg);
 			
+			String UPLOAD_URL = "http://lionlancer2k17.000webhostapp.com/index.php";
+
+			// Example data
+			//String username = "test_user_123";
+			//String datetime = "2016-12-09 10:00:00";
+			//File image = getImage();
+
+			// Create an HTTP client to execute the request
+			OkHttpClient client = new OkHttpClient();
+
+			// Create a multipart request body. Add metadata and files as 'data parts'.
+			RequestBody requestBody = new MultipartBody.Builder()
+					.setType(MultipartBody.FORM)
+					.addFormDataPart("counter", updateCount)
+					.addFormDataPart("message", msg)
+					//.addFormDataPart("image", image.getName(),
+							//RequestBody.create(MediaType.parse("image/jpeg"), image))
+					.build();
+
+			// Create a POST request to send the data to UPLOAD_URL
+			Request request = new Request.Builder()
+					.url(UPLOAD_URL)
+					.post(requestBody)
+					.build();
+
+			// Execute the request and get the response from the server
+			Response response = null;
+
+			try {
+				response = client.newCall(request).execute();
+				Log.d("MyService", "client.newCall(request).execute() OK");
+				Log.d("MyService", response);
+			} catch (IOException e) {
+				e.printStackTrace();
+				Log.d("MyService", "Error: " + e.getMessage());
+			}
+
+			// Check the response to see if the upload succeeded
+			if (response == null || !response.isSuccessful()) {
+				Log.d("MyService", "Unable to upload to server.");
+			} else {
+				Log.d("MyService", "Upload was successful.");
+			}
 			
+			
+			
+			/*
 			//if(this.userID != ""){
 				try{
 					//url = new URL("https://onesignal-3cdb8.firebaseio.com/users/"+this.userID+".json");
@@ -90,6 +136,26 @@ public class MyService extends BackgroundService {
 				http.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
 				
 				try{
+					output = new BufferedOutputStream(http.getOutputStream());
+
+					BufferedWriter writer = new BufferedWriter (new OutputStreamWriter(output, "UTF-8"));
+
+					String postdata = "counter="+updateCount+"&message="+msg;
+					
+					postdata = URLEncoder.encode(postdata, "UTF-8")
+					
+					writer.write(postdata);
+
+					writer.flush();
+					writer.close();
+					
+					output.close();
+				}catch(){
+					Log.d("MyService", "Error: " + e.getMessage());
+					result.put("Message", "IOException thrown: " + e.getMessage());
+				}
+				
+				try{
 					//con.connect();
 					http.connect();
 					Log.d("MyService", "http.connect(): OK");
@@ -100,20 +166,21 @@ public class MyService extends BackgroundService {
 				//try(OutputStream os = http.getOutputStream()) {
 				//	os.write(out);
 				//}
-				try{
+				
+				//try{
 					//os = http.getOutputStream();
 					//os.write(out);
 					
-					String postdata = "counter="+updateCount+"&message="+msg;
+				//	String postdata = "counter="+updateCount+"&message="+msg;
 					
-					os = new OutputStreamWriter(http.getOutputStream(), "8859_1");   
-					os.write(postdata);  
+				//	os = new OutputStreamWriter(http.getOutputStream(), "8859_1");   
+				//	os.write(postdata);  
 					
-					Log.d("MyService", "os.write(out): OK");
-				}catch(Exception e){
-					Log.d("MyService", "Error: " + e.getMessage());
-					result.put("Message", "Exception thrown: " + e.getMessage());
-				}
+				//	Log.d("MyService", "os.write(out): OK");
+				//}catch(Exception e){
+				//	Log.d("MyService", "Error: " + e.getMessage());
+				//	result.put("Message", "Exception thrown: " + e.getMessage());
+				//}
 				
 				// Do something with http.getInputStream()
 				
@@ -134,6 +201,7 @@ public class MyService extends BackgroundService {
 					result.put("Message", "IOException thrown: " + e.getMessage());
 				}
 			//}
+			*/
 		} catch (JSONException e) {
 		}
 		
