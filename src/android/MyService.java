241e212a710e158c23d8cb3d6be53f2494c840b6
Lionlancer <lionlancer@gmail.com>
2018-03-23 01:12:06 +0800

    updating post

--- src/android/MyService.java
+++ src/android/MyService.java
@@ -5,9 +5,12 @@ import java.io.*;
 import java.lang.String;
 import java.nio.charset.*;
 import java.text.SimpleDateFormat;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.Date;
 import java.net.URL;
 import java.net.URLConnection;
+import javax.net.ssl.HttpsURLConnection;
 
 //import com.google.auth.oauth2.GoogleCredentials;
 
@@ -19,81 +22,48 @@ import android.util.Log;
 import com.red_folder.phonegap.plugin.backgroundservice.BackgroundService;
 
 public class MyService extends BackgroundService {
-	
+
 	private final static String TAG = MyService.class.getSimpleName();
-	
+
 	private String mHelloTo = "World";
 	private String userID = "";
 	private Number updateCount = 0;
-	
+
 	@Override
 	protected JSONObject doWork() {
 		JSONObject result = new JSONObject();
-		
-		URL url = null;
+
+		HashMap map = new HashMap();
+
+		//URL url = null;
+		String url = "";
 		URLConnection con = null;
 		HttpURLConnection http  = null;
 		//byte[] out = null;
 		//int length = null;
 		OutputStreamWriter os = null;
-		
+
 		try {
-			
-			SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss"); 
-			String now = df.format(new Date(System.currentTimeMillis())); 
+
+			SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+			String now = df.format(new Date(System.currentTimeMillis()));
 
 			String msg = "Hello " + this.mHelloTo + " - its currently " + now;
 			result.put("Message", msg);
 
 			Log.d(TAG, msg);
 			Log.d("MyService", msg);
-			
-			String UPLOAD_URL = "http://lionlancer2k17.000webhostapp.com/index.php";
 
-			// Example data
-			//String username = "test_user_123";
-			//String datetime = "2016-12-09 10:00:00";
-			//File image = getImage();
+			url = "http://lionlancer2k17.000webhostapp.com/";
 
-			// Create an HTTP client to execute the request
-			OkHttpClient client = new OkHttpClient();
 
-			// Create a multipart request body. Add metadata and files as 'data parts'.
-			RequestBody requestBody = new MultipartBody.Builder()
-					.setType(MultipartBody.FORM)
-					.addFormDataPart("counter", updateCount)
-					.addFormDataPart("message", msg)
-					//.addFormDataPart("image", image.getName(),
-							//RequestBody.create(MediaType.parse("image/jpeg"), image))
-					.build();
+			map.put("counter", updateCount+"");
+			map.put("message", msg);
+			//map.put("key3", "value3");
 
-			// Create a POST request to send the data to UPLOAD_URL
-			Request request = new Request.Builder()
-					.url(UPLOAD_URL)
-					.post(requestBody)
-					.build();
+			performPostCall(url, map);
 
-			// Execute the request and get the response from the server
-			Response response = null;
 
-			try {
-				response = client.newCall(request).execute();
-				Log.d("MyService", "client.newCall(request).execute() OK");
-				Log.d("MyService", response);
-			} catch (IOException e) {
-				e.printStackTrace();
-				Log.d("MyService", "Error: " + e.getMessage());
-			}
-
-			// Check the response to see if the upload succeeded
-			if (response == null || !response.isSuccessful()) {
-				Log.d("MyService", "Unable to upload to server.");
-			} else {
-				Log.d("MyService", "Upload was successful.");
-			}
-			
-			
-			
 			/*
 			//if(this.userID != ""){
 				try{
@@ -103,18 +73,18 @@ public class MyService extends BackgroundService {
 				}catch(MalformedURLException e){
 					Log.d("MyService", "Error: " + e.getMessage());
 					result.put("Message", "MalformedURLException thrown: " + e.getMessage());
-					
+
 				}
 				try{
-					con = url.openConnection();		
+					con = url.openConnection();
 					Log.d("MyService", "url.openConnection: OK");
 				}catch(IOException e){
 					Log.d("MyService", "Error: " + e.getMessage());
 					result.put("Message", "IOException thrown: " + e.getMessage());
 				}
-				
+
 				http = (HttpURLConnection)con;
-				
+
 				try{
 					http.setRequestMethod("POST"); // PUT is another valid option
 					Log.d("MyService", "http.setRequestMethod: OK");
@@ -134,27 +104,27 @@ public class MyService extends BackgroundService {
 				//http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
 				//http.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
 				http.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
-				
+
 				try{
 					output = new BufferedOutputStream(http.getOutputStream());
 
 					BufferedWriter writer = new BufferedWriter (new OutputStreamWriter(output, "UTF-8"));
 
 					String postdata = "counter="+updateCount+"&message="+msg;
-					
+
 					postdata = URLEncoder.encode(postdata, "UTF-8")
-					
+
 					writer.write(postdata);
 
 					writer.flush();
 					writer.close();
-					
+
 					output.close();
 				}catch(){
 					Log.d("MyService", "Error: " + e.getMessage());
 					result.put("Message", "IOException thrown: " + e.getMessage());
 				}
-				
+
 				try{
 					//con.connect();
 					http.connect();
@@ -166,26 +136,26 @@ public class MyService extends BackgroundService {
 				//try(OutputStream os = http.getOutputStream()) {
 				//	os.write(out);
 				//}
-				
+
 				//try{
 					//os = http.getOutputStream();
 					//os.write(out);
-					
+
 				//	String postdata = "counter="+updateCount+"&message="+msg;
-					
-				//	os = new OutputStreamWriter(http.getOutputStream(), "8859_1");   
-				//	os.write(postdata);  
-					
+
+				//	os = new OutputStreamWriter(http.getOutputStream(), "8859_1");
+				//	os.write(postdata);
+
 				//	Log.d("MyService", "os.write(out): OK");
 				//}catch(Exception e){
 				//	Log.d("MyService", "Error: " + e.getMessage());
 				//	result.put("Message", "Exception thrown: " + e.getMessage());
 				//}
-				
+
 				// Do something with http.getInputStream()
-				
+
 				try{
-					
+
 					BufferedReader r = new BufferedReader(new InputStreamReader(http.getInputStream(), Charset.forName("UTF-8")));
 
 					StringBuilder sb = new StringBuilder();
@@ -204,19 +174,86 @@ public class MyService extends BackgroundService {
 			*/
 		} catch (JSONException e) {
 		}
-		
-		return result;	
+
+		return result;
 	}
+
+	public String  performPostCall(String requestURL, HashMap<String, String> postDataParams) {
+
+		URL url;
+		String response = "";
+		try {
+			url = new URL(requestURL);
+
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setReadTimeout(15000);
+			conn.setConnectTimeout(15000);
+			conn.setRequestMethod("POST");
+			conn.setDoInput(true);
+			conn.setDoOutput(true);
+			
+			Log.d("MyService", "POSTING TO: " + requestURL);
+			Log.d("MyService", "POSTING DATA: " + postDataParams);
+
+			OutputStream os = conn.getOutputStream();
+			BufferedWriter writer = new BufferedWriter(
+					new OutputStreamWriter(os, "UTF-8"));
+			writer.write(getPostDataString(postDataParams));
+
+			writer.flush();
+			writer.close();
+			os.close();
+			int responseCode=conn.getResponseCode();
+
+			if (responseCode == HttpsURLConnection.HTTP_OK) {
+				String line;
+				BufferedReader br=new BufferedReader(new InputStreamReader(conn.getInputStream()));
+				while ((line=br.readLine()) != null) {
+					response+=line;
+				}
+				
+				Log.d("MyService", "RESPONSE: " + response);
+				
+			}
+			else {
+				response="";
+				Log.d("MyService", "NO RESPONSE:");
+				
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return response;
+	}
+
+	private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException{
+		StringBuilder result = new StringBuilder();
+		boolean first = true;
+		for(Map.Entry<String, String> entry : params.entrySet()){
+			if (first)
+				first = false;
+			else
+				result.append("&");
+
+			result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+			result.append("=");
+			result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+		}
+
+		return result.toString();
+	}
+
 
 	@Override
 	protected JSONObject getConfig() {
 		JSONObject result = new JSONObject();
-		
+
 		try {
 			result.put("HelloTo", this.mHelloTo);
 		} catch (JSONException e) {
 		}
-		
+
 		return result;
 	}
 
@@ -225,16 +262,16 @@ public class MyService extends BackgroundService {
 		try {
 			if (config.has("HelloTo"))
 				this.mHelloTo = config.getString("HelloTo");
-			
+
 			if (config.has("userID"))
 				this.userID = config.getString("userID");
-			
+
 			if (config.has("updateCount"))
 				this.updateCount = config.getInt("updateCount");
 		} catch (JSONException e) {
 		}
-		
-	}     
+
+	}
 
 	@Override
 	protected JSONObject initialiseLatestResult() {
@@ -245,13 +282,13 @@ public class MyService extends BackgroundService {
 	@Override
 	protected void onTimerEnabled() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	protected void onTimerDisabled() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	/*
